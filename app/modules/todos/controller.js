24ceb7c17b37582cd32b66c8302e5d5565508701
Nancy Majors <nmajors@gmail.com>
2016-04-18 16:56:51 -0500

    finished adventurer mode

--- app/modules/todos/controller.js
+++ app/modules/todos/controller.js
@@ -2,6 +2,7 @@ class TodosController {
   constructor($firebaseArray){
     let ref = new Firebase ("https://nms-todo.firebaseio.com/");
     this.todos = $firebaseArray(ref);
+    this.changeView = "viewIncomplete";
     // this.item="";
   }
 
@@ -17,6 +18,14 @@ class TodosController {
     todo.completed = !todo.completed;
     console.log(todo);
     this.todos.$save(todo);
+  }
+
+  archiveTodos(){
+    this.todos.forEach((todo) =>{
+      if (todo.completed === true){
+        this.todos.$remove(todo);
+      }
+    })
   }
 }
 

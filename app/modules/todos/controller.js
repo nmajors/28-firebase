2b67c5d40a3e2563bca8c02431cab0c2cb3860f4
Nancy Majors <nmajors@gmail.com>
2016-04-29 11:21:04 -0500

    ux. showing and hiding icons and setting timeout

--- app/modules/todos/controller.js
+++ app/modules/todos/controller.js
@@ -1,32 +1,55 @@
 class TodosController {
-  constructor($firebaseArray){
+  constructor($firebaseArray, $timeout){
     let ref = new Firebase ("https://nms-todo.firebaseio.com/");
     this.todos = $firebaseArray(ref);
     this.changeView = "viewIncomplete";
+    this._$timeout = $timeout;
     // this.item="";
   }
 
   addTodo(){
     this.todos.$add({
       item: this.item,
-      completed: false
+      completed: false,
+      markedComplete: false,
+      markedDelete: false,
     });
     this.item = "";
   }
 
   toggleCompleted(todo){
-    todo.completed = !todo.completed;
+    todo.markedComplete = !todo.markedComplete;
     console.log(todo);
-    this.todos.$save(todo);
-  }
+      this._$timeout(()=>{
+        if (todo.markedComplete == true) {
+          todo.completed = true;
+          console.log(todo);
+          this.todos.$save(todo);
+        }
+        // todo.markedComplete = false;
+      }, 1000);
+
+    }
+
+
 
   archiveTodos(){
     this.todos.forEach((todo) =>{
       if (todo.completed === true){
         this.todos.$remove(todo);
       }
-    })
+    });
   }
+
+  deleteTodo(todo){
+    todo.markedDelete = !todo.markedDelete;
+      this._$timeout(()=>{
+        if (todo.markedDelete == true){
+          this.todos.$remove(todo);
+        }
+      }, 1000);
+      console.log(this.todo);
+    };
 }
 
 export default TodosController;
